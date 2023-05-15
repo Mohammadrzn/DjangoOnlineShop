eedc5f81354d2d0a6b592d2aa940bdaeec0cf7c7
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-15 19:35:11 +0330

    created_at commented (I think I don't need it now)

--- Core/models.py
+++ Core/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now, null=False, blank=False)
+    # created_at = models.DateTimeField(auto_now_add=True, default=timezone.now, null=False, blank=False)
     is_deleted = models.BooleanField("Is Deleted", default=False, null=False, blank=False)
 
     class Meta:
