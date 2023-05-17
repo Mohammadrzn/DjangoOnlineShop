a108a1effb8333938b25599f4de636ce011c7250
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-17 17:16:22 +0330

    verbose name & def __str__ added to models, also fild title added to Comment table

--- Product/models.py
+++ Product/models.py
@@ -6,6 +6,12 @@ from django.db import models
 class Category(BaseModel):
     name = models.CharField("Name", max_length=100, null=False, blank=False)
 
+    class Meta:
+        verbose_name = "Categories"
+
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Product(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
@@ -15,8 +21,21 @@ class Product(BaseModel):
     brand = models.CharField("Brand", max_length=75, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
 
+    class Meta:
+        verbose_name = "Products"
+
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Comment(BaseModel):
+    title = models.CharField("Title", max_length=100, null=False, blank=False)
     content = models.TextField("Content", null=False, blank=False)
     user = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
     product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name = "Comments"
+
+    def __str__(self):
+        return f"{self.title}"
