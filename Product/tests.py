f71c258b477ea29c6e253bfdada61756103f024f
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-17 22:26:05 +0330

    function test_create_category created

--- Product/tests.py
+++ Product/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Category
 
-# Create your tests here.
+
+class CategoryTest(TestCase):
+
+    def setUp(self) -> None:
+        self.category = Category(name="test")
+
+    def test_create_category(self):
+        self.assertEqual(self.category.name, "test")
+        self.assertFalse(self.category.is_deleted)
