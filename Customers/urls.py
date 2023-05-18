0b64e4f50508ea8438f785501a4e89f1b3fa002c
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-18 21:44:29 +0330

    Add url contact us page

--- Customers/urls.py
+++ Customers/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/', views.show_profile, name='profile')
+    path('profile/', views.show_profile, name='profile'),
+    path('login/', views.login, name='login'),
+    path('contact_us/', views.contact, name='contact_us'),
 ]
