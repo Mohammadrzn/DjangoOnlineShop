53ff3246603e323f920961facd84f6ed657021e0
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-19 00:01:23 +0330

    Creat test table Product & Comment

--- Product/tests.py
+++ Product/tests.py
@@ -1,12 +1,27 @@
 from django.test import TestCase
-from .models import Category
+from .models import Category, Product, Comment
 
 
 class CategoryTest(TestCase):
 
     def setUp(self) -> None:
-        self.category = Category(name="test")
+        self.product = Product.objects.create(name="test", price=150.5, count=5, brand="test", description="test",
+                                              is_sold_out=False)
+        self.category = Category.objects.create(name="test")
+        self.comment = Comment.objects.create(title="test", content="test & test")
+
+    def test_create_product(self):
+        self.assertEqual(self.product.name, "test")
+        self.assertEqual(self.product.price, 150.5)
+        self.assertEqual(self.product.count, 5)
+        self.assertEqual(self.product.brand, "test")
+        self.assertEqual(self.product.description, "test")
+        self.assertEqual(self.product.is_sold_out, False)
 
     def test_create_category(self):
         self.assertEqual(self.category.name, "test")
         self.assertFalse(self.category.is_deleted)
+
+    def test_create_comment(self):
+        self.assertEqual(self.product.title, "test")
+        self.assertEqual(self.product.content, "test & test")
