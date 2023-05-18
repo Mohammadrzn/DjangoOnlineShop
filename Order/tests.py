f79f347caeff9251cabf83755a4bd5bb014afff1
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-18 23:51:17 +0330

    Creat test table order

--- Order/tests.py
+++ Order/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Order
 
-# Create your tests here.
+
+class TestOrder(TestCase):
+
+    def setUp(self) -> None:
+        self.order = Order.objects.create(registration_date="2023-05-18 20:04:49", delivery_date="2023-05-18",
+                                          address="test")
+
+    def test_create_order(self):
+        self.assertEqual(self.order.registration_date, "2023-05-18 20:04:49")
+        self.assertEqual(self.order.delivery_date, "2023-05-18")
+        self.assertEqual(self.order.address, "test")
