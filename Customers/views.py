0a0b304f57130288c61fcabe033806398a02c668
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-21 16:32:10 +0330

    Add signup page

--- Customers/views.py
+++ Customers/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .forms import SignupForm
 
 
 def show_profile(request):
     return render(request, "profile.html")
+
+
+def signup(request):
+    form = SignupForm
+
+    return render(request, "signup.html", {
+        "form": form
+    })
 
 
 def login(request):
