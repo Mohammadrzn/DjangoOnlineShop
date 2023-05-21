5b87181d6c534ce84f04573c9bc76f739e0b5d0d
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-21 16:32:30 +0330

    Add signup url

--- Customers/urls.py
+++ Customers/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('profile/', views.show_profile, name='profile'),
     path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
     path('contact_us/', views.contact, name='contact_us'),
 ]
