3c81879b0f4f6b97cfbe9062badcca774cfc05c1
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-21 18:38:40 +0330

    Remove is_sold_out & use count instead

--- Core/views.py
+++ Core/views.py
@@ -3,5 +3,5 @@ from Product.models import Product, Category
 
 
 def home(request):
-    products = Product.objects.filter(is_sold_out=False)[0:6]
+    products = Product.objects.filter(count__gt=0)[0:6]
     return render(request, "home.html", {"products": products})
