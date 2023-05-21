05300283ab942990f411a91d9ebc9f95800518ed
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-21 18:38:17 +0330

    Remove is_sold_out & use count instead

--- Product/views.py
+++ Product/views.py
@@ -4,7 +4,7 @@ from .models import Product
 
 def detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    related_products = Product.objects.filter(category=product.category, is_sold_out=False).exclude(pk=pk)[0:9]
+    related_products = Product.objects.filter(category=product.category, count__gt=0).exclude(pk=pk)[0:9]
     return render(request, "detail.html", {
         "product": product,
         "related_products": related_products
