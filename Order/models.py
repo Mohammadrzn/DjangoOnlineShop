a897088b8e4d61af056cd536c27c26ed952500a5
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-21 22:02:58 +0330

    Delete OrderProduct table and add product many to many field

--- Order/models.py
+++ Order/models.py
@@ -9,17 +9,10 @@ class Order(BaseModel):
     registration_date = models.DateTimeField("Registration Date", auto_now_add=timezone.now, null=False, blank=False)
     delivery_date = models.DateField("Delivery Date", null=False, blank=False)
     address = models.TextField("Address", null=False, blank=False)
+    product = models.ManyToManyField("Product", null=False, blank=False)
 
     class Meta:
         ordering = ["registration_date"]
 
     def __str__(self):
         return f"{self.customer}"
-
-
-class OrderProduct(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.DO_NOTHING)
-    product = models.ForeignKey("Product", null=False, blank=False, on_delete=models.DO_NOTHING)
-
-    class Meta:
-        abstract = True
