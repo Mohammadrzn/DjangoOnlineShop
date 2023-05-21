03b3078dc43b7d964255262d758dbc14562a8dd4
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-21 20:57:39 +0330

    Add signup form to signup function

--- Customers/views.py
+++ Customers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SignupForm
 
 
@@ -7,7 +7,13 @@ def show_profile(request):
 
 
 def signup(request):
-    form = SignupForm
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = SignupForm
 
     return render(request, "signup.html", {
         "form": form
