4d62f02880ec71224f4973aeb3558bc018f30d51
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-22 16:57:54 +0330

    Remove jdatetime from registration_date and add auto_now_add=True instead

--- Order/models.py
+++ Order/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from Core.models import BaseModel
 from Customers.models import Customer
 from Product.models import Product
-import jdatetime
 
 
 class Order(BaseModel):
     customer = models.ForeignKey(Customer, null=False, blank=False, on_delete=models.CASCADE)
-    registration_date = models.DateTimeField("تاریخ ثبت", default=jdatetime.datetime.now, null=False, blank=False)
+    registration_date = models.DateTimeField("تاریخ ثبت", auto_now_add=True, null=False, blank=False)
     delivery_date = models.DateField("تاریخ دریافت", null=False, blank=False)
     address = models.TextField("آدرس", null=False, blank=False)
     product = models.ManyToManyField(Product, related_name="order_product")
