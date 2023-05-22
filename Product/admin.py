74d0f21ec1ef74a389012dac01b920a4ff68af01
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-22 22:18:28 +0330

    Add decorator for showing persian date

--- Product/admin.py
+++ Product/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Product, Category, Comment
 
-
 admin.site.register(Product)
-admin.site.register(Category)
 admin.site.register(Comment)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "jdatetime"]
