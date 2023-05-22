ed6dfbfa27ea1549123f4abad83b022ddd93bf04
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-22 22:15:46 +0330

    Add function to calendar in persian form on admin panel

--- Core/models.py
+++ Core/models.py
@@ -1,5 +1,6 @@
 import datetime
 from django.db import models
+from jalali_date import datetime2jalali
 
 
 class BaseModel(models.Model):
@@ -7,6 +8,11 @@ class BaseModel(models.Model):
     edited_at = models.DateTimeField(default=datetime.datetime.now, null=True, blank=True)
     deleted_at = models.DateTimeField(default=datetime.datetime.now, null=True, blank=True)
     is_deleted = models.BooleanField("Is Deleted", default=False, null=False, blank=False)
+
+    def jdatetime(self):
+        return datetime2jalali(self.created_at).strftime('%Y/%m/%d %H:%M:%S')
+
+    jdatetime.short_description = "تاریخ"
 
     class Meta:
         abstract = True
