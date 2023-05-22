dff81d33ab6165bfd05ee2d061678c7ee214724b
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-22 16:58:28 +0330

    Remove jdatetime and use datetime instead

--- Core/models.py
+++ Core/models.py
@@ -1,11 +1,11 @@
+import datetime
 from django.db import models
-import jdatetime
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(default=jdatetime.datetime.now, null=True, blank=True)
-    edited_at = models.DateTimeField(default=jdatetime.datetime.now, null=True, blank=True)
-    deleted_at = models.DateTimeField(default=jdatetime.datetime.now, null=True, blank=True)
+    created_at = models.DateTimeField(default=datetime.datetime.now, null=True, blank=True)
+    edited_at = models.DateTimeField(default=datetime.datetime.now, null=True, blank=True)
+    deleted_at = models.DateTimeField(default=datetime.datetime.now, null=True, blank=True)
     is_deleted = models.BooleanField("Is Deleted", default=False, null=False, blank=False)
 
     class Meta:
