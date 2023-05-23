bd8cc208ab28b5292a06db6c49a2a3c266dca5b4
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-23 17:05:27 +0330

    Add persian verbose names

--- Core/models.py
+++ Core/models.py
@@ -3,10 +3,10 @@ from jalali_date import datetime2jalali
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(null=True, blank=True)
-    edited_at = models.DateTimeField(null=True, blank=True)
-    deleted_at = models.DateTimeField(null=True, blank=True)
-    is_deleted = models.BooleanField("Is Deleted", default=False, null=False, blank=False)
+    created_at = models.DateTimeField("ایجاد شده در", null=True, blank=True)
+    edited_at = models.DateTimeField("ویراش شده در", null=True, blank=True)
+    deleted_at = models.DateTimeField("پاک شده در", null=True, blank=True)
+    is_deleted = models.BooleanField("حذف شده", default=False, null=False, blank=False)
 
     def jdatetime(self):
         return datetime2jalali(self.created_at).strftime('%Y/%m/%d %H:%M:%S')
