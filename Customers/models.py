6c61781f2af53350ea5b2d1d46c08efb37a792f5
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-23 12:59:30 +0330

    Add Some verbose name & change help_texts & verbose names to Persian

--- Customers/models.py
+++ Customers/models.py
@@ -4,20 +4,20 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Customer(AbstractUser, BaseModel):
-    mobile = models.SmallIntegerField("Mobile", help_text="Example: 9123456789", null=True, blank=True)
-    telephone = models.SmallIntegerField(help_text="Enter without your city code", null=True, blank=True)
-    national_id = models.CharField("National ID", max_length=10, null=True, blank=True)
-    age = models.SmallIntegerField("Age", null=True, blank=True)
+    mobile = models.SmallIntegerField("موبایل", help_text="مثال: 9123456789", null=True, blank=True)
+    telephone = models.SmallIntegerField(help_text="شماره کامل همراه با کد شهر", null=True, blank=True)
+    national_id = models.CharField("کد ملی", max_length=10, null=True, blank=True)
+    age = models.SmallIntegerField("سن", null=True, blank=True)
 
     GENDER_CHOICES = (
-        ("F", "Femail"),
-        ("M", "Mail")
+        ("F", "آقا"),
+        ("M", "خانم")
     )
-    gender = models.CharField("Gender", choices=GENDER_CHOICES, max_length=1, null=True, blank=True)
+    gender = models.CharField("جنسیت", choices=GENDER_CHOICES, max_length=1, null=True, blank=True)
 
     class Meta:
-        verbose_name = "Customer"
-        verbose_name_plural = "Customers"
+        verbose_name = "کاربر"
+        verbose_name_plural = "کاربران"
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -25,14 +25,15 @@ class Customer(AbstractUser, BaseModel):
 
 class Address(BaseModel):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    state = models.CharField("State", max_length=75, null=False, blank=False)
-    city = models.CharField("City", max_length=100, null=False, blank=False)
-    full_address = models.TextField("Full Address", null=False, blank=False)
-    postal_code = models.SmallIntegerField("Postal Code", null=False, blank=False)
+    state = models.CharField("استان", max_length=75, null=False, blank=False)
+    city = models.CharField("شهر", max_length=100, null=False, blank=False)
+    full_address = models.TextField("آدرس", help_text="آدرس کامل همراه با شماره پلاک و واحد", null=False, blank=False)
+    postal_code = models.SmallIntegerField("کد پستی", null=False, blank=False)
     is_deleted = None
 
     class Meta:
-        verbose_name = "Address"
+        verbose_name = "آدرس"
+        verbose_name_plural = "آدرس ها"
 
     def __str__(self):
         return f"{self.customer_id}"
