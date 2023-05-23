cc85012de8925242814c726c2240aa15dc902ce9
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-23 21:22:14 +0330

    Add decorator for showing dates in persian calendar

--- Customers/admin.py
+++ Customers/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Customer, Address
 
-admin.site.register(Customer)
 admin.site.register(Address)
+
+
+@admin.register(Customer)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["get_full_name", "persian_created_at", "persian_edited_at", "persian_deleted_at"]
