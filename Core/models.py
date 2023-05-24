be39626ae2bd216e9f8486276a82429d9ca87b31
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-24 08:24:52 +0330

    Add function to showing persian_created_at, persian_edited_at & persian_deleted_at separated

--- Core/models.py
+++ Core/models.py
@@ -8,10 +8,20 @@ class BaseModel(models.Model):
     deleted_at = models.DateTimeField("پاک شده در", null=True, blank=True)
     is_deleted = models.BooleanField("حذف شده", default=False, null=False, blank=False)
 
-    def jdatetime(self):
+    def persian_created_at(self):
         return datetime2jalali(self.created_at).strftime('%Y/%m/%d %H:%M:%S')
 
-    jdatetime.short_description = "تاریخ"
+    def persian_edited_at(self):
+        return datetime2jalali(self.edited_at).strftime('%Y/%m/%d %H:%M:%S')
+
+    def persian_deleted_at(self):
+        return datetime2jalali(self.deleted_at).strftime('%Y/%m/%d %H:%M:%S')
+
+    persian_created_at.short_description = "تاریخ ایجاد"
+
+    persian_edited_at.short_description = "تاریخ ویرایش"
+
+    persian_deleted_at.short_description = "تاریخ حذف"
 
     class Meta:
         abstract = True
