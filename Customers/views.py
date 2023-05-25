ff43b1365778ad84dc667dd1560a0574165b415e
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-25 22:49:53 +0330

    Change signup function to serialize data and save them if form & serializer are valid

--- Customers/views.py
+++ Customers/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
+from .serializers import CustomerCreateSerializer
+from djoser.views import UserViewSet
+from rest_framework.permissions import AllowAny
+from .models import Customer
+
+
+class CustomerViewSet(UserViewSet):
+    queryset = Customer.objects.all()
+    permission_classes = [AllowAny]
 
 
 def show_profile(request):
@@ -7,17 +16,22 @@ def show_profile(request):
 
 
 def signup(request):
-    if request.method == "POST":
+    errors = {}
+    if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("home")
+            serializer = CustomerCreateSerializer(data=form.cleaned_data)
+            if serializer.is_valid():
+                serializer.save()
+                return redirect('login')
+            else:
+                errors = serializer.errors
+        else:
+            errors = form.errors
     else:
-        form = SignupForm
+        form = SignupForm()
 
-    return render(request, "signup.html", {
-        "form": form
-    })
+    return render(request, 'signup.html', {'form': form, 'errors': errors})
 
 
 def login(request):
