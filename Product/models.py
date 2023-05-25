27134a02ec00c149a05c8f80ca88d5927ce15d60
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-26 00:04:04 +0330

    Change verbose names to Persian

--- Product/models.py
+++ Product/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Category(BaseModel):
-    name = models.CharField("Name", max_length=100, null=False, blank=False)
+    name = models.CharField("نام", max_length=100, null=False, blank=False)
     category = models.ForeignKey("self", on_delete=models.CASCADE)
 
     class Meta:
@@ -17,12 +17,12 @@ class Category(BaseModel):
 
 class Product(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, related_name="category_product")
-    name = models.CharField("Name", max_length=100, null=False, blank=False)
-    price = models.FloatField("Price", null=False, blank=False)
+    name = models.CharField("نام", max_length=100, null=False, blank=False)
+    price = models.FloatField("قیمت", null=False, blank=False)
     count = models.SmallIntegerField(null=False, blank=False)
-    brand = models.CharField("Brand", max_length=75, null=False, blank=False)
-    description = models.TextField("Description", null=False, blank=False)
-    image = models.ImageField("Image", upload_to="Product_images", null=True, blank=True)
+    brand = models.CharField("برند", max_length=75, null=False, blank=False)
+    description = models.TextField("توضیحات", null=False, blank=False)
+    image = models.ImageField("تضویر", upload_to="Product_images", null=True, blank=True)
 
     class Meta:
         ordering = ["name"]
@@ -33,13 +33,13 @@ class Product(BaseModel):
 
 
 class Comment(BaseModel):
-    title = models.CharField("Title", max_length=100, null=False, blank=False)
-    content = models.TextField("Content", null=False, blank=False)
+    title = models.CharField("موضوع", max_length=100, null=False, blank=False)
+    content = models.TextField("متن نظر", null=False, blank=False)
     user = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
     product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
 
     class Meta:
-        verbose_name_plural = "کامنت ها"
+        verbose_name_plural = "نظرات"
 
     def __str__(self):
         return f"{self.title}"
