ab3033714de1fb7fae7233fec3d4f380a281bf87
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-26 00:50:26 +0330

    Add view for check user is authenticated or not

--- Customers/views.py
+++ Customers/views.py
@@ -6,6 +6,9 @@ from rest_framework.permissions import AllowAny
 from .models import Customer
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class CustomerViewSet(UserViewSet):
@@ -59,6 +62,12 @@ def my_login(request):
         form = LoginForm()
 
     return render(request, 'login.html', {'form': form, 'errors': errors})
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def check_auth(request):
+    return Response({'message': 'Authenticated'}, status=200)
 
 
 def contact(request):
