168ba1376d9c7565aa1f1bcd2e593292835597c5
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-25 22:37:34 +0330

    Change default values and use custom fields for SignupForm & LoginForm

--- Customers/forms.py
+++ Customers/forms.py
@@ -1,8 +1,17 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django import forms
 
 
-class SignupForm(UserCreationForm):
+class SignupForm(forms.Form):
+    username = forms.CharField(max_length=128)
+    password = forms.CharField(widget=forms.PasswordInput)
+
     class Meta:
-        model = User
-        fields = ("username", "password1", "password2")
+        fields = ("username", "password")
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=128)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        fields = ("username", "password")
