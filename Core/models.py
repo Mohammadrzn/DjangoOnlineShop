dc808340d81c41fa2948d9fb1c06e6d4122f6f99
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-25 22:24:47 +0330

    Add if to persian calendar functions to prevent an error

--- Core/models.py
+++ Core/models.py
@@ -3,19 +3,26 @@ from jalali_date import datetime2jalali
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField("ایجاد شده در", null=True, blank=True)
+    created_at = models.DateTimeField("ایجاد شده در",auto_now_add=True, null=True, blank=True)
     edited_at = models.DateTimeField("ویراش شده در", null=True, blank=True)
     deleted_at = models.DateTimeField("پاک شده در", null=True, blank=True)
     is_deleted = models.BooleanField("حذف شده", default=False, null=False, blank=False)
 
     def persian_created_at(self):
-        return datetime2jalali(self.created_at).strftime('%Y/%m/%d %H:%M:%S')
+        if self.created_at:
+            return datetime2jalali(self.created_at).strftime('%Y/%m/%d %H:%M:%S')
+        return None
 
     def persian_edited_at(self):
-        return datetime2jalali(self.edited_at).strftime('%Y/%m/%d %H:%M:%S')
+        if self.edited_at:
+            return datetime2jalali(self.edited_at).strftime('%Y/%m/%d %H:%M:%S')
+        else:
+            return None
 
     def persian_deleted_at(self):
-        return datetime2jalali(self.deleted_at).strftime('%Y/%m/%d %H:%M:%S')
+        if self.deleted_at:
+            return datetime2jalali(self.deleted_at).strftime('%Y/%m/%d %H:%M:%S')
+        return None
 
     persian_created_at.short_description = "تاریخ ایجاد"
 
