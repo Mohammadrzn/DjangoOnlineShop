ab251d48f7e0f60c40de2c65cccf34ccb6aec4e6
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-25 23:46:29 +0330

    Change name of login view

--- Customers/urls.py
+++ Customers/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.show_profile, name='profile'),
-    path('login/', views.login, name='login'),
+    path('login/', views.my_login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('contact_us/', views.contact, name='contact_us'),
 ]
