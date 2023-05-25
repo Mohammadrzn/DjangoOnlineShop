43a7cc09c0bcb430b1937df7caecd6fa03ca9366
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-26 00:35:01 +0330

    Add login function that use jwt to login

--- Customers/views.py
+++ Customers/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import SignupForm
+from .forms import SignupForm, LoginForm
 from .serializers import CustomerCreateSerializer
 from djoser.views import UserViewSet
 from rest_framework.permissions import AllowAny
 from .models import Customer
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class CustomerViewSet(UserViewSet):
@@ -34,8 +36,29 @@ def signup(request):
     return render(request, 'signup.html', {'form': form, 'errors': errors})
 
 
-def login(request):
-    return render(request, "login.html")
+def my_login(request):
+    errors = {}
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = get_user_model().objects.get(username=username)
+
+            if user.check_password(password):
+                refresh = RefreshToken.for_user(user)
+                token = str(refresh.access_token)
+                request.session['jwt_token'] = token
+
+                return redirect('home')
+            else:
+                errors['authentication'] = 'Invalid username or password'
+        else:
+            errors = form.errors
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form, 'errors': errors})
 
 
 def contact(request):
