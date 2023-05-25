c31ca7f479b55e984b7b0b128899e16cae23764e
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-26 00:50:55 +0330

    Add url check_auth function

--- Customers/urls.py
+++ Customers/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('profile/', views.show_profile, name='profile'),
     path('login/', views.my_login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('check-auth/', views.check_auth, name='check_auth'),
     path('contact_us/', views.contact, name='contact_us'),
 ]
