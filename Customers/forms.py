624471f28cf4f07e9d9e8741df2c98fc47f948f5
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-25 23:47:41 +0330

    Add clean function to handling the login better

--- Customers/forms.py
+++ Customers/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Customer
 
 
 class SignupForm(forms.Form):
@@ -12,6 +13,21 @@ class SignupForm(forms.Form):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=128)
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            try:
+                customer = Customer.objects.get(username=username)
+                if not customer.check_password(password):
+                    raise forms.ValidationError('نام کاربری یا رمز اشتباه است')
+            except Customer.DoesNotExist:
+                raise forms.ValidationError('نام کاربری یا رمز اشتباه است')
+
+        return cleaned_data
 
     class Meta:
         fields = ("username", "password")
