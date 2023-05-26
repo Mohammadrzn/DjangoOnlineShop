68e67cd89734bfc4caf641d1699424c2b4787fa6
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-26 10:04:42 +0330

    Add verbose name for cont field

--- Product/models.py
+++ Product/models.py
@@ -19,7 +19,7 @@ class Product(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, related_name="category_product")
     name = models.CharField("نام", max_length=100, null=False, blank=False)
     price = models.FloatField("قیمت", null=False, blank=False)
-    count = models.SmallIntegerField(null=False, blank=False)
+    count = models.SmallIntegerField("تعداد باقی مانده", null=False, blank=False)
     brand = models.CharField("برند", max_length=75, null=False, blank=False)
     description = models.TextField("توضیحات", null=False, blank=False)
     image = models.ImageField("تضویر", upload_to="Product_images", null=True, blank=True)
