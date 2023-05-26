8a7b0e619f77207eac2a0a868a98ba0051e85e2c
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-26 14:01:08 +0330

    Add test for __str__ functions

--- Product/tests.py
+++ Product/tests.py
@@ -5,8 +5,7 @@ from .models import Category, Product, Comment
 class CategoryTest(TestCase):
 
     def setUp(self) -> None:
-        self.product = Product.objects.create(name="test", price=150.5, count=5, brand="test", description="test",
-                                              is_sold_out=False)
+        self.product = Product.objects.create(name="test", price=150.5, count=5, brand="test", description="test")
         self.category = Category.objects.create(name="test")
         self.comment = Comment.objects.create(title="test", content="test & test")
 
@@ -16,7 +15,6 @@ class CategoryTest(TestCase):
         self.assertEqual(self.product.count, 5)
         self.assertEqual(self.product.brand, "test")
         self.assertEqual(self.product.description, "test")
-        self.assertEqual(self.product.is_sold_out, False)
 
     def test_create_category(self):
         self.assertEqual(self.category.name, "test")
@@ -25,3 +23,12 @@ class CategoryTest(TestCase):
     def test_create_comment(self):
         self.assertEqual(self.product.title, "test")
         self.assertEqual(self.product.content, "test & test")
+
+    def test_str_product(self):
+        self.assertNotEqual(str(self.product), "not_test")
+
+    def test_str_category(self):
+        self.assertNotEqual(str(self.category), "not_test")
+
+    def test_str_comment(self):
+        self.assertNotEqual(str(self.comment), "not_test")
