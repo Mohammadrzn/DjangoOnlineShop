ec801b10194432e8ee9a51e4a6f8e2cefd25a27b
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-26 09:56:29 +0330

    Add url for showing main categories in dropdown menu. Add url for showing products of one category

--- Product/urls.py
+++ Product/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "product"
 urlpatterns = [
     path('<int:pk>/', views.detail, name="detail"),
+    path('categories/', views.main_category, name="category"),
+    path('categories/products/<int:pk>/', views.category_product, name="product_category"),
 ]
