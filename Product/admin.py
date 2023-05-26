69cfb3fc8d844478c88d8aa7d814fc95b135d3f0
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-26 10:06:10 +0330

    Add custom field show class for products in admin panel

--- Product/admin.py
+++ Product/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Product, Category, Comment
 
-admin.site.register(Product)
 admin.site.register(Comment)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["name", "persian_created_at", "persian_edited_at"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "count", "persian_created_at", "persian_edited_at"]
