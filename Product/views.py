5887715d18509a57a395c5e8c167e85597c11902
Mohammad Bagher <mohammadbagherrezanejad@gmail.com>
2023-05-26 09:56:05 +0330

    Add view for showing main categories in dropdown menu. Add view for showing products of one category

--- Product/views.py
+++ Product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 
 def detail(request, pk):
@@ -8,4 +8,21 @@ def detail(request, pk):
     return render(request, "detail.html", {
         "product": product,
         "related_products": related_products
+    })
+
+
+def main_category(request):
+    categories = Category.objects.filter(inner_category=None)
+    return render(request, 'home.html', {
+        'categories': categories,
+    })
+
+
+def category_product(request, pk):
+    category = Category.objects.all()
+    product = get_object_or_404(Product, pk=pk)
+    category_products = Product.objects.filter(category=product.category, count__gt=0).exclude(pk=pk)
+    return render(request, "category.html", {
+        "category_products": category_products,
+        "category": category
     })
