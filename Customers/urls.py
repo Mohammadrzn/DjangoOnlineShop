e39032e1bd370af36c37a500f3f8771da3e3674e
mohammad <mohammadbagherrezanejad@gmail.com>
2023-05-30 23:26:52 +0330

    Add logout url

--- Customers/urls.py
+++ Customers/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.show_profile, name='profile'),
+    path('logout/', views.Logout.as_view(), name='logout'),
     path('login/', views.Login.as_view(), name='login'),
     path('signup/', views.Signup.as_view(), name='signup'),
     path('user/', views.CustomerView.as_view(), name='user'),
