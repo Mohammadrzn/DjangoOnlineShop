59151cdc8163b88a9d629939c95b949eadc2667d
mohammad <mohammadbagherrezanejad@gmail.com>
2023-05-30 19:51:31 +0330

    Change urls for class base views

--- Customers/urls.py
+++ Customers/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.show_profile, name='profile'),
-    path('login/', views.my_login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('check-auth/', views.check_auth, name='check_auth'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('signup/', views.Signup.as_view(), name='signup'),
     path('contact_us/', views.contact, name='contact_us'),
 ]
