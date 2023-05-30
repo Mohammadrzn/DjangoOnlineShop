2998cbd5bcd99a05955a1c8835b854568d64e5ec
mohammad <mohammadbagherrezanejad@gmail.com>
2023-05-30 21:15:48 +0330

    Add url to see logged_in user information

--- Customers/urls.py
+++ Customers/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('profile/', views.show_profile, name='profile'),
     path('login/', views.Login.as_view(), name='login'),
     path('signup/', views.Signup.as_view(), name='signup'),
+    path('user/', views.CustomerView.as_view(), name='user'),
     path('contact_us/', views.contact, name='contact_us'),
 ]
