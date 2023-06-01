0fdb7dec536e54b9b56696f89810b9df2ef1bc64
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-01 23:56:51 +0330

    Add app name

--- Customers/urls.py
+++ Customers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "auth"
 urlpatterns = [
     path('profile/', views.show_profile, name='profile'),
     path('logout/', views.Logout.as_view(), name='logout'),
