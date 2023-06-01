98629437f985456bfa5d626f583d58786eae2de2
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-01 23:42:36 +0330

    Add app name

--- Product/urls.py
+++ Product/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('<int:pk>/', views.detail, name="detail"),
     path('categories/', views.main_category, name="category"),
     path('categories/products/<int:pk>/', views.category_product, name="product_category"),
-    path('order/',views.order_page, name="order_page"),
+    path('order/', views.order_page, name="order_page"),
 ]
