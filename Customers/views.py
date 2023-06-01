0cecbbd88015fcd08d27df1f9b3b063221db3e57
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-02 01:32:06 +0330

    Change expiration time to 70 seconds

--- Customers/views.py
+++ Customers/views.py
@@ -45,7 +45,7 @@ class Login(APIView):
 
         payload = {
             "id": user.id,
-            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=10),
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=70),
             "iat": datetime.datetime.utcnow()
         }
 
