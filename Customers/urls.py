906be1c0adf2ceba01544dce2cb4bcb493cd6739
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-05 17:03:01 +0330

    Add url for changing User's information

--- Customers/urls.py
+++ Customers/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = "auth"
 urlpatterns = [
     path('profile/', views.show_profile, name='profile'),
+    path('change/', views.Change.as_view(), name='change'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('login/', views.Login.as_view(), name='login'),
     path('signup/', views.Signup.as_view(), name='signup'),
-    path('user/', views.CustomerView.as_view(), name='user'),
     path('contact_us/', views.contact, name='contact_us'),
 ]
