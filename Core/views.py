f701894a2273864eba94a918413d95fdb894964b
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-06 18:16:10 +0330

    Add discount_product to get method to show discounted products on home page, Change view from function to class

--- Core/views.py
+++ Core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 from Product.models import Product, Category
 
 
-def home(request):
-    products = Product.objects.filter(count__gt=0)[0:6]
-    return render(request, "home.html", {"products": products})
+class Home(APIView):
+    @staticmethod
+    def get(request):
+        new_products = Product.objects.filter(count__gt=0, discount_percent=0)[0:6]
+        discount_product = Product.objects.filter(count__gt=0, discount_percent__gt=0)
+
+        return render(request, "home.html", {
+            "new_products": new_products,
+            "discount_product": discount_product
+        })
