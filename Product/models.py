a33128311383759edca7b1d3075c11ca70a6df12
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-06 18:11:02 +0330

    Add discount_percent & method get_price for showing the price after discount

--- Product/models.py
+++ Product/models.py
@@ -24,7 +24,9 @@ class Product(BaseModel):
     count = models.SmallIntegerField("تعداد باقی مانده", null=False, blank=False)
     brand = models.CharField("برند", max_length=75, null=False, blank=False)
     description = models.TextField("توضیحات", null=False, blank=False)
-    image = models.ImageField("تضویر", upload_to="Product_images", null=True, blank=True)
+    image = models.ImageField("تصویر", upload_to="Product_images", null=True, blank=True)
+    discount_percent = models.SmallIntegerField("درصد تخفیف", null=False, blank=False,
+                                                help_text="بدون علمات درصد وارد شود، اگر نخفیف ندارد 0 وارد کنید")
 
     def image_tag(self):
         return mark_safe('<img src="/media/%s" width="150" height="150" />' % self.image)
@@ -32,6 +34,14 @@ class Product(BaseModel):
     image_tag.allow_tags = True
 
     image_tag.short_description = 'تصویر محصول'
+
+    def get_price(self):
+        if self.discount_percent == 0:
+            return self.price
+        else:
+            return self.price - (self.discount_percent / 100 * self.price)
+
+    get_price.short_description = "قیمت پس از تخفیف"
 
     class Meta:
         ordering = ["name"]
