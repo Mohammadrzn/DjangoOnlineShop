28ef9ae2a8aac7b15227107c6f3e3b8229a70893
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-06 18:16:32 +0330

    Add discount_product class home urls

--- DjangoOnlineShop/urls.py
+++ DjangoOnlineShop/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from Core.views import home
+from Core.views import Home
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', home, name="home"),
+                  path('', Home.as_view(), name="home"),
                   path('auth/', include('Customers.urls')),
                   path('product/', include("Product.urls")),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
