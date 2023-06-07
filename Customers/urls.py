1a947358c5edfdfe0c986a452af5bf216998765a
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-07 21:33:05 +0330

    Add url addresses

--- Customers/urls.py
+++ Customers/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('logout/', views.Logout.as_view(), name='logout'),
     path('login/', views.Login.as_view(), name='login'),
     path('signup/', views.Signup.as_view(), name='signup'),
+    path('addresses/', views.Address.as_view(), name='addresses'),
     path('contact_us/', views.contact, name='contact_us'),
 ]
