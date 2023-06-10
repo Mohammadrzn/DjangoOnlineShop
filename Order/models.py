35347f1218907102faebbc16ce880660bdc21182
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-10 16:09:47 +0330

    Add OrderItems class

--- Order/models.py
+++ Order/models.py
@@ -9,6 +9,7 @@ class Order(BaseModel):
     registration_date = models.DateTimeField("تاریخ ثبت", auto_now_add=True, null=False, blank=False)
     delivery_date = models.DateField("تاریخ دریافت", null=False, blank=False)
     address = models.TextField("آدرس", null=False, blank=False)
+    status = models.BooleanField("وضعیت سفارش", default=False)
     product = models.ManyToManyField(Product, related_name="order_product")
 
     class Meta:
@@ -19,8 +20,20 @@ class Order(BaseModel):
         return f"{self.customer}"
 
 
-class Cart(BaseModel):
-    created_at = None
-    edited_at = None
-    deleted_at = None
-    product = models.ManyToManyField(Product, related_name="cart_product")
+class OrderItems(models.Model):
+    order = models.ForeignKey(Order, related_name='order_items', verbose_name="اقلام سفارش", on_delete=models.CASCADE)
+    count = models.IntegerField("تعداد")
+    product = models.ForeignKey(Product, verbose_name="محصول", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "اقلام سفارش"
+        verbose_name_plural = 'اقلام فاکتور'
+
+    def __str__(self) -> str:
+        return f"{self.Product.name}"
+
+    def item_cost(self):
+        return self.count * self.Product.price
+
+    def item_discount(self):
+        return self.Product.total_discount * self.count
