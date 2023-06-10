605e22546e2769d7441dca0781d666cf0a787154
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-10 15:27:18 +0330

    Add SendOTPForm for sending OTP codes & VerificationForm to verify codes

--- Customers/forms.py
+++ Customers/forms.py
@@ -31,3 +31,17 @@ class LoginForm(forms.Form):
 
     class Meta:
         fields = ("username", "password")
+
+
+class SendOTPForm(forms.Form):
+    mail_phone = forms.CharField(error_messages={
+        'required': 'وارد کردن این فیلد الزامی',
+        'invalid': 'شماره همراه یا ایمیل معتبر نمی باشد.'
+    })
+
+
+class VerificationForm(forms.Form):
+    verification_code = forms.CharField(max_length=6, error_messages={
+        'required': 'وارد کردن این فیلد الزامی',
+        'max_length': 'کد وارد شده بیش از حد مجاز می باشد.'
+    })
