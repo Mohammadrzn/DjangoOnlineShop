d93d639ae94193667f760ae02799652e3c1dd7d0
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-10 15:28:23 +0330

    Remove Signup and Login form

--- Customers/forms.py
+++ Customers/forms.py
@@ -1,36 +1,4 @@
 from django import forms
-from .models import Customer
-
-
-class SignupForm(forms.Form):
-    username = forms.CharField(max_length=128)
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        fields = ("username", "password")
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=128)
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        username = cleaned_data.get('username')
-        password = cleaned_data.get('password')
-
-        if username and password:
-            try:
-                customer = Customer.objects.get(username=username)
-                if not customer.check_password(password):
-                    raise forms.ValidationError('نام کاربری یا رمز اشتباه است')
-            except Customer.DoesNotExist:
-                raise forms.ValidationError('نام کاربری یا رمز اشتباه است')
-
-        return cleaned_data
-
-    class Meta:
-        fields = ("username", "password")
 
 
 class SendOTPForm(forms.Form):
