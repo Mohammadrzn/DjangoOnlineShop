112904c317e6b90b6b3a0014f076116820b55a7a
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-10 17:19:25 +0330

    Add verbose name for status field

--- Core/views.py
+++ Core/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from Product.models import Product, Category
+from Product.models import Product, Category, Discount
 
 
 class Home(APIView):
     @staticmethod
     def get(request):
-        new_products = Product.objects.filter(count__gt=0, discount_percent=0)[0:6]
-        discount_product = Product.objects.filter(count__gt=0, discount_percent__gt=0)
+        new_products = Product.objects.filter(count__gt=0, )[0:6]
+        discount_product = Product.objects.filter(count__gt=0, discount=True)
+        discount = Discount.objects.get()
 
         return render(request, "home.html", {
             "new_products": new_products,
-            "discount_product": discount_product
+            "discount_product": discount_product,
+            "discount": discount,
         })
