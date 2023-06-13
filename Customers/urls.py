67be8ec6d73c3fba018f1fffe0c336b4c9ff9bf3
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-13 19:41:34 +0330

    Add information url

--- Customers/urls.py
+++ Customers/urls.py
@@ -5,6 +5,7 @@ app_name = "auth"
 urlpatterns = [
     path('profile/', views.ShowProfile.as_view(), name='profile'),
     path('change/', views.Change.as_view(), name='change'),
+    path('information/', views.Information.as_view(), name='information'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('login/', views.Login.as_view(), name='login'),
     path('signup/', views.Signup.as_view(), name='signup'),
