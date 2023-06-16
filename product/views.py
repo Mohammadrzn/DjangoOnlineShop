948b4652fc656c65621fd07cd7f0c451ded81b12
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-17 02:23:21 +0330

    Combine three class to one

--- product/views.py
+++ product/views.py
@@ -35,45 +35,65 @@ def category_product(request, pk):
     })
 
 
-class CartShow(APIView):
-    @staticmethod
-    def get(request):
-        return render(request, "cart.html")
+class CartView(APIView):
+    def get(self, request):
+        cart = Cart(request)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
 
-
-class CartAddView(APIView):
-    @staticmethod
-    def post(request, product_id):
+    def post(self, request, product_id):
         cart = Cart(request)
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product, quantity=1)
+        quantity = request.data.get('quantity', 1)
+        cart.add(product, quantity=quantity)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
+
+    def patch(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        action = request.data.get('action')
+
+        if action == 'increment':
+            cart.add(product, quantity=1)
+        elif action == 'decrement':
+            quantity = cart.cart.get(str(product.id), {}).get('quantity', 0)
+            if quantity > 1:
+                cart.add(product, quantity=-1)
+            else:
+                cart.remove(product)
+
+        total_price = cart.get_total_price()
+        total_discount = cart.get_total_discount()
+        total_price_discount = total_price - total_discount
+
+        serializer = CartSerializer(cart)
+        return Response({
+            'cart': serializer.data,
+            'total_price': total_price,
+            'total_discount': total_discount,
+            'total_price_discount': total_price_discount,
+        })
+
+    def delete(self, request, product_id=None):
+        cart = Cart(request)
+
+        if product_id is not None:
+            product = get_object_or_404(Product, id=product_id)
+            cart.remove(product)
+        else:
+            cart.clear()
+
         serializer = CartSerializer(cart)
         return Response(serializer.data)
 
 
-class CartMinesView(APIView):
-    @staticmethod
-    def get(request, product_id):
-        cart = Cart(request)
-        product = get_object_or_404(Product, id=product_id)
-        cart.add(product, quantity=-1)
-        serializer = CartSerializer(cart)
-        return Response(serializer.data)
-
-
-class CartRemoveView(APIView):
-    @staticmethod
-    def get(request, product_id):
-        cart = Cart(request)
-        product = get_object_or_404(Product, id=product_id)
-        cart.remove(product)
-        serializer = CartSerializer(cart)
-        return Response(serializer.data)
+def cart_view(request):
+    return render(request, "cart.html")
 
 
 class ClearCartView(APIView):
-    @staticmethod
-    def get(request):
+    def delete(self, request):
         cart = Cart(request)
         cart.clear()
         serializer = CartSerializer(cart)
