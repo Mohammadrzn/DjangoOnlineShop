7887d1b4914f796e648dbfd7551f00a10a63fcd9
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-17 02:21:07 +0330

    Add some serializers

--- order/serializers.py
+++ order/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from .models import Order
-from customers.models import Customer
+from .models import Order, OrderItems, Product
+from customers.models import Customer, Address
 
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -9,11 +9,30 @@ class CustomerSerializer(serializers.ModelSerializer):
         fields = ['get_full_name']
 
 
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['name', 'price', 'discount_price', 'total_discount']
+
+
+class OrderItemsSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
+    class Meta:
+        model = OrderItems
+        fields = ['id', 'order', 'product', 'count']
+
+
 class OrderSerializer(serializers.ModelSerializer):
     customer = CustomerSerializer()
+    order_items = OrderItemsSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
         fields = ['id', 'customer', 'address', 'status', 'order_items']
 
 
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
