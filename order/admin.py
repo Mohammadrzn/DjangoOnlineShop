a30ab383a07e9de5462da3501624978b432215a9
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-17 01:31:13 +0330

    Add OrderItems to admin panel

--- order/admin.py
+++ order/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, OrderItems, Carts
+
+
+admin.site.register([Carts])
 
 
 @admin.register(Order)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["customer", "persian_created_at", "persian_edited_at", "persian_deleted_at"]
+
+
+@admin.register(OrderItems)
+class OrderItems(admin.ModelAdmin):
+    list_display = ['id', 'product', 'item_cost']
+    list_filter = ['product']
+    search_fields = ['product']
+    ordering = ['id']
+
+    @staticmethod
+    def product(obj):
+        return obj.product.name
