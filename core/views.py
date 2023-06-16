231e010bca69d99287801f81fb5524fe49fab044
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-16 12:09:15 +0330

    Add showing categories

--- core/views.py
+++ core/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
+from django.views import View
 from product.models import Product, Category, Discount
 
 
-class Home(APIView):
+class Home(View):
     @staticmethod
     def get(request):
-        new_products = Product.objects.filter(count__gt=0, )[0:6]
+        new_products = Product.objects.filter(count__gt=0, discount_id=None)[0:6]
         discount_product = Product.objects.filter(count__gt=0, discount=True)
         discount = Discount.objects.get(discount_for="p")
+        category = Category.objects.values()
 
         return render(request, "home.html", {
             "new_products": new_products,
             "discount_product": discount_product,
             "discount": discount,
+            "category": category,
         })
