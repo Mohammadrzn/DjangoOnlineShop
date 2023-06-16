2137427754a986e0d9a90e4623cb8f43b3f47951
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-17 02:21:43 +0330

    Change urls

--- product/urls.py
+++ product/urls.py
@@ -7,9 +7,8 @@ urlpatterns = [
     path('categories/', views.main_category, name="category"),
     path('categories/products/<int:pk>/', views.category_product, name="product_category"),
     path('order/', views.order_page, name="order_page"),
-    path('cart/', views.CartShow.as_view(), name='cart'),
-    path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='add'),
-    path('cart/mines/<int:product_id>/', views.CartMinesView.as_view(), name='mines'),
-    path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='remove'),
+    path('', views.CartView.as_view(), name='cart'),
+    path('cart/<int:product_id>/', views.CartView.as_view(), name='cart-product'),
+    path('cart/', views.cart_view, name='cart_page'),
     path('clear/', views.ClearCartView.as_view(), name='clear'),
 ]
