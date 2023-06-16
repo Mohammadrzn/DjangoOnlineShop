3d905d142b4d0474fe6a11457fd792a509844fce
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-16 10:02:24 +0330

    Remove normal urls

--- Customers/api_urls.py
+++ Customers/api_urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
 from . import api_views
 
-
 app_name = "api"
 
 urlpatterns = [
-    path('profile/', api_views.ShowProfile.as_view(), name='profile'),
+    path('login/', api_views.Login.as_view(), name='login'),
+    path('signup/', api_views.Signup.as_view(), name='signup'),
     path('change/', api_views.Change.as_view(), name='change'),
-    path('information/', api_views.Information.as_view(), name='information'),
-    path('logout/', api_views.Logout.as_view(), name='logout'),
     path('login/otp/', api_views.Otp.as_view(), name='otp'),
     path('verification/', api_views.Verification.as_view(), name='verification'),
-    path('addresses/', api_views.Address.as_view(), name='addresses'),
     path('address/change/', api_views.ChangeAddress.as_view(), name='change_address'),
 ]
