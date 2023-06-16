80384cde2a45d8ac68adf3e928ead0669c63d538
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-16 10:01:03 +0330

    Add some urls

--- Customers/urls.py
+++ Customers/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 app_name = "auth"
 urlpatterns = [
+    path('logout/', views.logout, name='logout'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('contact_us/', views.contact, name='contact_us'),
+    path('profile/', views.show_profile, name='profile'),
+    path('information/', views.information, name='information'),
+    path('addresses/', views.address, name='addresses'),
 ]
 
