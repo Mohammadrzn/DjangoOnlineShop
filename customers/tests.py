454deea2f0c34a3f16889e179d04c7ad32117884
mohammad <mohammadbagherrezanejad@gmail.com>
2023-06-30 20:30:44 +0330

    Add more tests for models

--- customers/tests.py
+++ customers/tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from .models import Address
 
 
-class UsersTest(TestCase):
+class TestUsers(TestCase):
     """
     Unittest class for test creation of Customer model and superuser, and their methods
     """
@@ -14,28 +14,29 @@ class UsersTest(TestCase):
         """
         creating the models requirements for testing
         """
-        self.user = get_user_model().objects.create_user(edited_at=now(), deleted_at=now(), password="test",
+        self.user = get_user_model().objects.create_user(edited_at=now(), deleted_at=now(), password="test_password",
                                                          username="test_user_name", first_name="test_first_name",
                                                          last_name="test_last_name", email="test@user.com",
                                                          mobile=9123456789, telephone=66554433,
                                                          national_id="0123456789", age=20, role="A", gender="M")
         self.superuser = get_user_model().objects.create_superuser(username="admin", email="test@admin.com",
-                                                                   password="test")
+                                                                   password="test_password_admin")
 
     def test_create_user(self):
         """
         test creation of Customer model
         """
+        # Running the tests takes some time, so the times will not be equal and will differ by a few milliseconds
         self.assertNotEqual(self.user.edited_at, now())
         self.assertNotEqual(self.user.deleted_at, now())
-        self.assertTrue(self.user.password, check_password("test", self.user.password))
+        self.assertTrue(self.user.password, check_password("test_password", self.user.password))
         self.assertTrue(self.user.is_active)
-        self.assertNotEqual(self.user.username, "not_test_user_name")
-        self.assertNotEqual(self.user.first_name, "not_test_first_name")
-        self.assertNotEqual(self.user.last_name, "not_test_last_name")
-        self.assertNotEqual(self.user.email, "not_test@user.com")
+        self.assertEqual(self.user.username, "test_user_name")
+        self.assertEqual(self.user.first_name, "test_first_name")
+        self.assertEqual(self.user.last_name, "test_last_name")
+        self.assertEqual(self.user.email, "test@user.com")
         self.assertFalse(self.user.is_staff)
-        self.assertNotEqual(self.user.mobile, 9876543219)
+        self.assertEqual(self.user.mobile, 9123456789)
         self.assertEqual(self.user.telephone, 66554433)
         self.assertEqual(self.user.national_id, "0123456789")
         self.assertEqual(self.user.age, 20)
@@ -56,7 +57,7 @@ class UsersTest(TestCase):
         """
         self.assertEqual(self.superuser.username, "admin")
         self.assertEqual(self.superuser.email, "test@admin.com")
-        self.assertTrue(self.user.password, check_password("test", self.user.password))
+        self.assertTrue(self.user.password, check_password("test_password_admin", self.user.password))
         self.assertTrue(self.superuser.is_active)
         self.assertTrue(self.superuser.is_staff)
         self.assertTrue(self.superuser.is_superuser)
@@ -71,22 +72,25 @@ class TestAddress(TestCase):
         """
         creating the models requirements for testing
         """
-        self.user = get_user_model().objects.create_user(edited_at=now(), deleted_at=now(), password="test",
-                                                         username="test_user_name", role="A")
-        self.address = Address.objects.create(created_at=now(), edited_at=now(), customer=self.user, state="test",
-                                              city="test", full_address="test", postal_code=123456789)
+        self.user = get_user_model().objects.create_user(edited_at=now(), deleted_at=now(), password="test_password",
+                                                         username="test_username", role="A")
+        self.address = Address.objects.create(created_at=now(), edited_at=now(), customer=self.user, state="test_state",
+                                              city="test_city", full_address="test_full_address", postal_code=123456789,
+                                              is_default=True)
 
     def test_create_address(self):
         """
         test creation of Address model
         """
+        # Running the tests takes some time, so the times will not be equal and will differ by a few milliseconds
         self.assertNotEqual(self.address.created_at, now())
         self.assertNotEqual(self.address.edited_at, now())
         self.assertEqual(self.address.customer, self.user)
-        self.assertNotEqual(self.address.state, "not_test")
-        self.assertNotEqual(self.address.city, "not_test")
-        self.assertNotEqual(self.address.full_address, "not_test")
-        self.assertNotEqual(self.address.postal_code, 987654321)
+        self.assertEqual(self.address.state, "test_state")
+        self.assertEqual(self.address.city, "test_city")
+        self.assertEqual(self.address.full_address, "test_full_address")
+        self.assertEqual(self.address.postal_code, 123456789)
+        self.assertTrue(self.address.is_default)
 
     def test__str__address(self):
         """
